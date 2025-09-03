3b27a9db69bd3d9043d0decf6f932c7ed3a5ae74
havenbradshaw <havenjbradshaw@gmail.com>
2025-09-03 14:31:22 -0500

    Delete the obsolete "Tester" file

--- zoo/manager/Main.java
+++ zoo/manager/Main.java
@@ -7,6 +7,8 @@ import zoo.animals.Bird;
 import zoo.animals.Mammal;
 import zoo.animals.Reptile;
 
+/** This Main class adds objects to the array "animals" and puts this array into the ZooManager object 
+ * "zoo" as a parameter, allowing the array to be handled through the menu the ZooManager class sets up*/
 public class Main {
     public static void main(String[] args) {
         ArrayList<Animal> animals = new ArrayList<>();
